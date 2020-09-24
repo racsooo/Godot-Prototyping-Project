0bf53a7d9368b5c6464fd6158d5850d7b4e747f2
owong <owong@example.com>
2020-09-24 17:27:14 +0100

    added platformer player

--- Scripts/Platformer/Platformer_player.gd
+++ Scripts/Platformer/Platformer_player.gd
@@ -1,16 +1,39 @@
 extends KinematicBody2D
 
+var motion = Vector2()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var ACCELERATION = 60
+var MAX_SPEED = 80
+const FRICTION = 0.4
+const GRAVITY = 10
+const JUMP = 275
+const AIR_FRICTION = 0.2
+const UP = Vector2(0, -1)
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var idle = false
+	
+	motion.y += GRAVITY
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		
+	if Input.is_action_pressed("ui_left"):
+		motion.x = max(motion.x - ACCELERATION, - MAX_SPEED)
+		
+	if is_on_floor():
+		motion.y = 0
+			
+		if not Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+			idle = true
+			motion.x = lerp(motion.x, 0, FRICTION)	
+	
+		if Input.is_action_pressed("ui_up"):
+			motion.y -= JUMP
+			
+	if not is_on_floor():
+		if idle:
+			motion.x = lerp(motion.x, 0, AIR_FRICTION)
+	
+	
+	move_and_slide(motion, UP)
